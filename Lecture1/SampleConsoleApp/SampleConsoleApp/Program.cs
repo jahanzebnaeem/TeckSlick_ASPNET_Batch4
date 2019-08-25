eaaf6eb768fb8cf589db4f31739c74e2f6842353
jahanzebnaeem <jahanzeb.naeem@hotmail.com>
2019-08-25 17:56:04 +0500

    Lecture 1
    Console applicaion demo.

---
+++ Lecture1/SampleConsoleApp/SampleConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                // Iteraion 1
+                //if (i <= 5)
+                //{
+                //    Console.WriteLine("Hello to you and welcome to the first class of ASP.NET");
+
+                //}
+                //else
+                //{
+                //    Console.WriteLine("The value is greater than 5");
+                //}
+
+                // Iteration 2
+                string name = Console.ReadLine();
+                while (i <= 10)
+                {
+                    Console.WriteLine("S.No " + i + " - Welcome " + name + " to the Lecture 1 of ASP.NET");
+
+                    i++;
+                }
+            }
+        }
+    }
+}
