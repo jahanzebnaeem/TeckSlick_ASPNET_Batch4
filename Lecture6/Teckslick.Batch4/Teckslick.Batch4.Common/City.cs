2ce6ce55850b395b7f3405c9d891a2ccec72fb54
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2019-09-21 18:21:06 +0500

    POS Classes Creation
    Created classes according to the ERD from Lecture 5

---
+++ Lecture6/Teckslick.Batch4/Teckslick.Batch4.Common/City.cs
@@ -0,0 +1,8 @@
+﻿namespace Teckslick.Batch4.Common
+{
+    public class City
+    {
+        public int CityID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Lecture6/Teckslick.Batch4/Teckslick.Batch4.Common/Country.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Teckslick.Batch4.Common
+{
+    public class Country
+    {
+        public int CountryID { get; set; }
+        public string Name { get; set; }
+        public List<City> Cities { get; set; }
+    }
+}
