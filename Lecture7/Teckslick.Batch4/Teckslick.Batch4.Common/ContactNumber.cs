ebc7a118437eaf5231f516adebff73afd21eeda5
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2019-09-27 16:46:12 +0500

    Lecture 7 Code
    Thisi is the code for Lecture 7.

---
+++ Lecture7/Teckslick.Batch4/Teckslick.Batch4.Common/ContactNumber.cs
@@ -0,0 +1,28 @@
+﻿namespace Teckslick.Batch4.Common
+{
+    public class ContactNumber
+    {
+        public int ContactNumberID { get; set; }
+        public int CountryCode { get; set; }
+        public int NetworkCode { get; set; }
+        public int Number { get; set; }
+        public ContactNumberType ContactNumberType { get; set; }
+
+        #region Constructors
+        public ContactNumber() { }
+
+        public ContactNumber(int cc, int nc, int num, ContactNumberType cnt)
+        {
+            CountryCode = cc;
+            NetworkCode = nc;
+            Number = num;
+            ContactNumberType = cnt;
+        }
+
+        public ContactNumber(int nc, int num) : this(92, nc, num, ContactNumberType.Mobile) { }
+            #endregion
+
+            // +92-42-1234567/+92-300-1234567
+
+        }
+    }
---
+++ Lecture7/Teckslick.Batch4/Teckslick.Batch4.SalesDemo/SaleLineItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teckslick.Batch4.SalesDemo.Items;
+
+namespace Teckslick.Batch4.SalesDemo
+{
+    public class SaleLineItem
+    {
+        public int SaleLineItemID { get; set; }
+        public List<Item> Items { get; set; }
+        public int Quantity { get; set; }
+        public int UnitPrice { get; set; }
+    }
+}
