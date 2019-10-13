b663ea9b19d344d3b7774e7895558b91a2f7884f
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2019-10-13 09:07:54 +0500

    MVC Intro
    What is MVC?
    How it works?
    What is the difference between MVC and Web Forms?

---
+++ Lecture10/Teckslick.Batch4/Teckslick.Batch4.WinUI/Country.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Teckslick.Batch4.WinUI
+{
+    partial class frmCountry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdCountries = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grdCountries)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdCountries
+            // 
+            this.grdCountries.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdCountries.Location = new System.Drawing.Point(12, 12);
+            this.grdCountries.Name = "grdCountries";
+            this.grdCountries.Size = new System.Drawing.Size(240, 150);
+            this.grdCountries.TabIndex = 0;
+            // 
+            // frmCountry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grdCountries);
+            this.Name = "frmCountry";
+            this.Text = "Countries List";
+            ((System.ComponentModel.ISupportInitialize)(this.grdCountries)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdCountries;
+    }
+}
+
---
+++ Lecture10/Teckslick.Batch4/Teckslick.Batch4.WinUI/Country.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Teckslick.Batch4.Application;
+
+namespace Teckslick.Batch4.WinUI
+{
+    public partial class frmCountry : Form
+    {
+        public frmCountry()
+        {
+            InitializeComponent();
+            // Add logic to hook up with control
+            List<Teckslick.Batch4.Common.Country> countries = new CountryHandler().GetCountries();
+            grdCountries.DataSource = countries;
+        }
+    }
+}
