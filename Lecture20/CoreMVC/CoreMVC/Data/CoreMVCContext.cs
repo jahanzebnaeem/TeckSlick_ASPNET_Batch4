f387c8a815021e14ae38663200517e7e5663b1ef
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2019-11-22 15:47:14 +0500

    Core MVC - Intro
    This is the introduction to .NET Core MVC.

---
+++ Lecture20/CoreMVC/CoreMVC/Data/CoreMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreMVC.Models
+{
+    public class CoreMVCContext : DbContext
+    {
+        public CoreMVCContext (DbContextOptions<CoreMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CoreMVC.Models.Country> Country { get; set; }
+    }
+}
---
+++ Lecture20/CoreMVC/CoreMVC/Models/Country.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVC.Models
+{
+    public class Country
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
