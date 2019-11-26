cea430ac633f8653b6e30ae05db96aeea68c62a1
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2019-11-26 19:28:28 +0500

    Lecture 21-22
    Dependency Injection

---
+++ Lecture21-22/WebMVCCore/WebMVCCoreEmpty/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebMVCCoreEmpty.Models;
+
+// For more information on enabling MVC for empty projects, visit https: //go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebMVCCoreEmpty.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ICountryRepository _countryRepository;
+        public HomeController(ICountryRepository countryRepository)
+        {
+            _countryRepository = countryRepository;
+        }
+        // GET: /<controller>/
+        public string Index()
+        {
+            //return Json(new { ID = 1, Name = "Jahanzeb" });
+            return _countryRepository.GetCountry(1).Name;
+        }
+    }
+}
---
+++ Lecture21-22/WebMVCCore/WebMVCCoreEmpty/Models/MockCountryRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMVCCoreEmpty.Models
+{
+    public class MockCountryRepository : ICountryRepository
+    {
+        private List<Country> _countryList;
+
+        public MockCountryRepository()
+        {
+            _countryList = new List<Country>()
+            {
+                new Country() { ID = 1, Name = "Pakistan" },
+                new Country() { ID = 2, Name = "Iran" },
+                new Country() { ID = 3, Name = "Afghanistan" }
+            };
+        }
+
+        public Country GetCountry(int id)
+        {
+            return _countryList.FirstOrDefault(c => c.ID == id);
+        }
+    }
+}
